69e1f6ae55b6971de49541198753b17055b21f22
jack.hulbert@interworks.com <jack.hulbert@interworks.com>
2023-07-06 21:18:27 +0000

    First commit

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,23 +1,7 @@
-with source as (
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
 
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_orders') }}
-
-),
-
-renamed as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from source
-
-)
-
-select * from renamed
+from raw.jaffle_shop.orders
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,25 +1,11 @@
-with source as (
-    
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    select * from {{ ref('raw_payments') }}
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-),
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
-renamed as (
-
-    select
-        id as payment_id,
-        order_id,
-        payment_method,
-
-        -- `amount` is currently stored in cents, so we convert it to dollars
-        amount / 100 as amount
-
-    from source
-
-)
-
-select * from renamed
+from raw.stripe.payment 
